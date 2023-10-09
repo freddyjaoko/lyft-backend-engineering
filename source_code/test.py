a2f2596c7b6a6ad3ca82e3bdc6eab7b3ec661a45
fredjaoko <fredjaoko123@gmail.com>
2023-10-09 16:20:24 +0300

    tires update

--- source_code/test.py
+++ source_code/test.py
@@ -5,11 +5,12 @@ from engine.sternman_engine import SternmanEngine
 from engine.willoughby_engine import WilloughbyEngine
 from battery.Nubbin_battery import NubbinBattery
 from battery.Spindler_battery import SpindlerBattery
+from tires.carrigan_tire import CarriganTire
+from tires.octoprime_tire import OctoprimeTire
 
 
 
-
-
+#ENGINE TEST
 class TestCapuletEngine(unittest.TestCase):
     def test_needs_service_true(self):
         current_mileage = 30001
@@ -52,6 +53,7 @@ class TestWilloughbyEngine(unittest.TestCase):
 
 
 
+#BATTERY TEST
 class TestNubbinBattery(unittest.TestCase):
     def test_needs_service_true(self):
         current_date = date.fromisoformat("2020-05-15")
@@ -78,3 +80,29 @@ class TestSpindlerBattery(unittest.TestCase):
         last_service_date = date.fromisoformat("2019-01-10")
         battery = SpindlerBattery(current_date, last_service_date)
         self.assertFalse(battery.needs_service())
+
+
+
+#TIRE TEST
+class TestCarriganTires(unittest.TestCase):
+    def test_needs_service_true(self):
+        tire_wear = [0.1, 0.3, 0.2, 0.9]
+        tires = CarriganTire(tire_wear)
+        self.assertTrue(tires.needs_service())
+
+    def test_needs_service_false(self):
+        tire_wear = [0.1, 0.2, 0.4, 0.2]
+        tires = CarriganTire(tire_wear)
+        self.assertFalse(tires.needs_service())
+
+
+class TestOctoprimeTires(unittest.TestCase):
+    def test_needs_service_true(self):
+        tire_wear = [0.8, 0.8, 0.8, 0.7]
+        tires = OctoprimeTire(tire_wear)
+        self.assertTrue(tires.needs_service())
+
+    def test_needs_service_false(self):
+        tire_wear = [0.1, 0.2, 0.4, 0.2]
+        tires = OctoprimeTire(tire_wear)
+        self.assertFalse(tires.needs_service())
